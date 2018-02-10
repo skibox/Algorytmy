0f3ce862d01607ca1857d5842bc123f4209ccd76
Unknown <m.skibicki3007@gmail.com>
2018-02-10 13:13:10 +0100

    1002
    1WayList

--- src/PracaDomowa1702/zadanie3/SingleWayList.java
+++ src/PracaDomowa1702/zadanie3/SingleWayList.java
@@ -1,87 +1,106 @@
-//package PracaDomowa1702.zadanie3;
-//
-//public class SingleWayList {
-//	private int counter;
-//	private Element head;
-//	
-//	public int getCounter() {
-//		return counter;
-//	}
-//
-//	public Element getHead() {
-//		return head;
-//	}
-//
-//	public void addElementAtBeginning(int value) {
-//		Element newElement = new Element();
-//		Element lastElement = head;
-//		newElement.setData(value);
-//		
-//		if(counter > 0) {
-//			while(lastElement.getNext() != head)
-//				lastElement = lastElement.getNext();
-//			newElement.setNext(head);
-//			head.setNext(head.getNext());
-//			head = newElement;
-//			
-//			counter++;
-//		}
-//		else 
-//			setFirstElement(newElement);
-//	}
-//	
-//	public void addElementAtEnd(int value) {
-//		Element newElement = new Element();
-//		Element lastElement = head;
-//		newElement.setData(value);
-//		
-//		if(counter > 0) {
-//			
-//			while(lastElement.getNext() != head)
-//				lastElement = lastElement.getNext();
-//			
-//			lastElement.setNext(newElement);
-//			newElement.setNext(head);
-//			
-//			counter++;
-//		}
-//		else 
-//			setFirstElement(newElement);
-//	}
-//	
-//	public Element findForward(int value) {
-//		Element currentElement = head;
-//		
-//		while(currentElement != null) {
-//			if(currentElement.getData() == value)
-//				return currentElement;
-//			currentElement = currentElement.getNext();
-//		}
-//		
-//		return null;
-//		
-//	}
-//	
-//	private void setFirstElement(Element newElement) {
-//		head = newElement;
-//		newElement.setNext(head);
-//		counter++;
-//	}
-//	
-//	public void addElementAfter(int value, int afterWhat) throws Exception{
-//		Element elementAfter = findForward(afterWhat);
-//		Element newElement = new Element();
-//		newElement.setData(value);
-//		
-//		if (elementAfter == null) {
-//			throw new Exception("Nie ma takiej wartości.");
-//		}
-//
-//		else {
-//			newElement.setNext(elementAfter.getNext());
-//			elementAfter.setNext(newElement);
-//			counter++;
-//		}
-//		
-//	}
-//}
+package PracaDomowa1702.zadanie3;
+
+import Zajecia0302.MyList.Element;
+
+public class SingleWayList {
+	private int counter;
+	private Element head;
+	
+	public int getCounter() {
+		return counter;
+	}
+
+	public Element getHead() {
+		return head;
+	}
+
+	public void addElementAtBeginning(int value) {
+		Element newElement = new Element();
+		Element lastElement = head;
+		newElement.setData(value);
+		
+		if(counter > 0) {
+			while(lastElement.getNext() != head)
+				lastElement = lastElement.getNext();
+			newElement.setNext(head);
+			lastElement.setNext(newElement);
+			head = newElement;
+			
+			counter++;
+		}
+		else 
+			setFirstElement(newElement);
+	}
+	
+	public void addElementAtEnd(int value) {
+		Element newElement = new Element();
+		Element lastElement = head;
+		newElement.setData(value);
+		
+		if(counter > 0) {
+			
+			while(lastElement.getNext() != head)
+				lastElement = lastElement.getNext();
+			
+			lastElement.setNext(newElement);
+			newElement.setNext(head);
+			
+			counter++;
+		}
+		else 
+			setFirstElement(newElement);
+	}
+	
+	public Element findForward(int value) {
+		Element currentElement = head;
+		
+		while(currentElement != null) {
+			if(currentElement.getData() == value)
+				return currentElement;
+			currentElement = currentElement.getNext();
+		}
+		
+		return null;
+		
+	}
+	
+	private void setFirstElement(Element newElement) {
+		head = newElement;
+		newElement.setNext(head);
+		counter++;
+	}
+	
+	public void addElementAfter(int value, int afterWhat) throws Exception{
+		Element elementAfter = findForward(afterWhat);
+		Element newElement = new Element();
+		newElement.setData(value);
+		
+		if (elementAfter == null) {
+			throw new Exception("Nie ma takiej wartości.");
+		}
+
+		else {
+			newElement.setNext(elementAfter.getNext());
+			elementAfter.setNext(newElement);
+			counter++;
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sBuilder = new StringBuilder();
+		
+		Element currentElement = head;
+		
+		while(true) {
+			
+			sBuilder.append(currentElement.getData() + " ");
+			if(currentElement.getNext() == head) break;
+			currentElement = currentElement.getNext();
+			
+		} 
+		
+		return sBuilder.toString();	
+	}
+}
--- src/PracaDomowa1702/zadanie3/SingleWayListDemo.java
+++ src/PracaDomowa1702/zadanie3/SingleWayListDemo.java
@@ -1,27 +1,27 @@
-//package PracaDomowa1702.zadanie3;
-//
-//public class SingleWayListDemo {
-//	public static void main(String[] args) {
-//		SingleWayList newList = new SingleWayList();
-//		newList.addElementAtBeginning(10);
-//		newList.addElementAtBeginning(11);
-//		newList.addElementAtEnd(110);
-//		newList.addElementAtEnd(111);
-//		newList.addElementAtEnd(112);
-//		newList.addElementAtEnd(113);
-//		System.out.println();
+package PracaDomowa1702.zadanie3;
+
+public class SingleWayListDemo {
+	public static void main(String[] args) {
+		SingleWayList newList = new SingleWayList();
+		newList.addElementAtBeginning(10);
+		newList.addElementAtBeginning(11);
+		newList.addElementAtEnd(110);
+		newList.addElementAtEnd(111);
+		newList.addElementAtEnd(112);
+		newList.addElementAtEnd(113);
+		System.out.println();
+		System.out.println(newList.toString());
+//		newList.addElementAfter(1500100900, 10);
 //		System.out.println(newList.toString());
-////		newList.addElementAfter(1500100900, 10);
-////		System.out.println(newList.toString());
-////		newList.addElementAfter(314, 113);
-////		System.out.println(newList.toString());
-////		newList.addElementBefore(6969, 10);
-////		System.out.println(newList.toString());
-////		newList.deleteElement(6969);
-////		System.out.println(newList.toString());
-////		newList.deleteElement(110);
-////		System.out.println(newList.toString());
-////		newList.deleteElement(314);
-////		System.out.println(newList.toString());
-//	}
-//}
+//		newList.addElementAfter(314, 113);
+//		System.out.println(newList.toString());
+//		newList.addElementBefore(6969, 10);
+//		System.out.println(newList.toString());
+//		newList.deleteElement(6969);
+//		System.out.println(newList.toString());
+//		newList.deleteElement(110);
+//		System.out.println(newList.toString());
+//		newList.deleteElement(314);
+//		System.out.println(newList.toString());
+	}
+}
